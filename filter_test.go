0a0fc33e0f8e670ef5f71b9ed6f86ed972f6beff
Joni Kähärä <joni.kahara@async.fi>
2025-01-17 23:08:38 +0200

    NewFilter() test goes first

--- filter_test.go
+++ filter_test.go
@@ -6,6 +6,27 @@ import (
 	"testing"
 )
 
+func TestNewFilter(t *testing.T) {
+	type args struct {
+		config  Config
+		request *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+		want Filter
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewFilter(tt.args.config, tt.args.request); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewFilter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestFilter_filter(t *testing.T) {
 	type fields struct {
 		Enabled  bool
@@ -36,27 +57,6 @@ func TestFilter_filter(t *testing.T) {
 			}
 			if got := filter.filter(tt.args.spot); got != tt.want {
 				t.Errorf("filter() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestNewFilter(t *testing.T) {
-	type args struct {
-		config  Config
-		request *http.Request
-	}
-	tests := []struct {
-		name string
-		args args
-		want Filter
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewFilter(tt.args.config, tt.args.request); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewFilter() = %v, want %v", got, tt.want)
 			}
 		})
 	}
